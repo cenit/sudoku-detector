8272c8d7c3637283d53f1b541f4ac6d351f41a1f
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-11-13 12:34:28 +0100

    remove static build from CI, update include path to be more compatible with older releases

--- src/sudoku-tools.h
+++ src/sudoku-tools.h
@@ -4,12 +4,11 @@
 #pragma once
 
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 using namespace std;
 
 void drawLine(Vec2f line, Mat &img, Scalar rgb = CV_RGB(0, 0, 255));
 void mergeRelatedLines(vector<Vec2f> *lines, Mat &img);
-
